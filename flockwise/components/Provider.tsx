e41613b0827b28b0707d2cfada06c6017ceeac39
Arif <68075877+devAgant@users.noreply.github.com>
2023-11-01 02:41:08 -0500

    Added UI and basic user auth

---
+++ flockwise/components/Provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { SessionProvider } from 'next-auth/react';
+
+
+const Provider = ({ children, session }: any | null) => {
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default Provider
