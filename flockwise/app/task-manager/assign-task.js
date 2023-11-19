ff119b3e3195c94e47ae1640814c77585524f6d1
emw8105 <emw8105@gmail.com>
2023-11-19 17:00:00 -0600

    flawless assign task implementation

--- flockwise/app/task-manager/assign-task.js
+++ flockwise/app/task-manager/assign-task.js
@@ -1,9 +1,58 @@
 // Written by Evan
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Notification from '@components/Notification';
+import { useSession, getSession } from 'next-auth/react';
+import SessionManager from '@models/sessionManager';
 
 const AssignTask = () => {
   const [selectedEmployee, setSelectedEmployee] = useState('');
   const [selectedTask, setSelectedTask] = useState('');
+  const [employees, setEmployees] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [notification, setNotification] = useState(null);
+  const { data: session } = useSession();
+
+  const handleCloseNotification = () => {
+    setNotification(null);
+  };
+
+  useEffect(() => {
+    fetchEmployees();
+    fetchTasks();
+    SessionManager.setSession(session);
+  }, [session]);
+
+  const fetchEmployees = async () => {
+    try {
+      const response = await fetch('/api/getEmployees');
+      const data = await response.json();
+  
+      // Check if data is an array before calling map
+      if (Array.isArray(data)) {
+        setEmployees(data);
+      } else {
+        console.error('Error: Data is not an array', data);
+      }
+    } catch (error) {
+      console.error('Error fetching employees:', error);
+    }
+  };
+  
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch('/api/getTasks');
+      const data = await response.json();
+  
+      // Check if data is an array before calling map
+      if (Array.isArray(data.tasks)) {
+        setTasks(data.tasks);
+      } else {
+        console.error('Error: Data.tasks is not an array', data);
+      }
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
 
   const handleEmployeeChange = (event) => {
     setSelectedEmployee(event.target.value);
@@ -13,11 +62,52 @@ const AssignTask = () => {
     setSelectedTask(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Perform the task assignment logic here, e.g., update the database.
-    // Provide feedback to the user.
-  };
+    try {
+      // Perform the task assignment logic here
+      const session = await getSession();
+
+      const accessLevel = session?.user?.employee?.accessLevel;
+      if (accessLevel >= 2) {
+      const response = await fetch('/api/assignTask', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ selectedEmployee, selectedTask }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const responseData = await response.json();
+      console.log('Task assigned:', responseData);
+  
+      // Display a success notification
+      setNotification({
+        message: `Task ${selectedTask} assigned to employee ${selectedEmployee} successfully!`,
+        isError: false,
+      });
+  
+      // Reset the form or handle success
+      setSelectedEmployee('');
+      setSelectedTask('');
+    } else {
+      setNotification({
+        message: 'You do not have the required access to assign a task.',
+        isError: true,
+      });
+    }
+    } catch (error) {
+      console.error('Error:', error);
+  
+      setNotification({
+        message: 'Error assigning task. Please try again.',
+        isError: true,
+    })};
+  }
 
   const formStyles = {
     display: 'flex',
@@ -60,33 +150,44 @@ const AssignTask = () => {
         <div style={inputContainerStyles}>
           <label style={labelStyles}>Select Employee:</label>
           <select
-            value={selectedEmployee}
-            onChange={handleEmployeeChange}
-            style={inputStyles}
-          >
-            <option value="">Select an employee</option>
-            <option value="employee1">Employee 1</option>
-            <option value="employee2">Employee 2</option>
-            {/* Add more employee options here */}
-          </select>
+  value={selectedEmployee}
+  onChange={handleEmployeeChange}
+  style={inputStyles}
+>
+  <option value="">Select an employee</option>
+  {employees.map((employee) => (
+    <option key={employee.value} value={employee.value}>
+      {employee.label}
+    </option>
+  ))}
+</select>
         </div>
         <div style={inputContainerStyles}>
           <label style={labelStyles}>Select Task:</label>
           <select
-            value={selectedTask}
-            onChange={handleTaskChange}
-            style={inputStyles}
-          >
-            <option value="">Select a task</option>
-            <option value="task1">Task 1</option>
-            <option value="task2">Task 2</option>
-            {/* Add more task options here */}
-          </select>
+  value={selectedTask}
+  onChange={handleTaskChange}
+  style={inputStyles}
+>
+  <option value="">Select a task</option>
+  {tasks.map((task) => (
+    <option key={task.value} value={task.value}>
+      {task.label}
+    </option>
+  ))}
+</select>
         </div>
         <button type="submit" style={buttonStyles}>
           Assign Task
         </button>
       </form>
+      {notification && (
+        <Notification
+          message={notification.message}
+          isError={notification.isError}
+          onClose={handleCloseNotification}
+        />
+      )}
     </div>
   );
 };
---
+++ flockwise/pages/api/getTasks.js
@@ -0,0 +1,24 @@
+// pages/api/getTasks.js
+// Written by Evan
+import { connectToDB } from '@utils/database';
+import Task from '@models/task';
+
+export default async function handler(req, res) {
+  try {
+    await connectToDB();
+    const tasks = await Task.find({}, 'taskCode title');
+
+    // Log the tasks to the console for debugging
+    console.log('Tasks:', tasks);
+
+    // Ensure that the tasks array is structured properly
+    const formattedTasks = tasks.map((task) => ({
+      value: task.taskCode,
+      label: task.title,
+    }));
+
+    res.status(200).json({ success: true, tasks: formattedTasks });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+}
