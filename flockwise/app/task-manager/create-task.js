af91d2416ba8955ec570bd85c263a78f1f2ed8bf
Arif <68075877+devAgant@users.noreply.github.com>
2023-11-19 15:23:14 -0600

    fixed noti class

--- flockwise/app/task-manager/create-task.js
+++ flockwise/app/task-manager/create-task.js
@@ -9,7 +9,7 @@ import SessionManager from '@models/sessionManager';
 import NextAuth from 'next-auth/next';
 import { connectToDB } from '@utils/database';
 import sessionManager from '@models/sessionManager';
-import Notification from '@components/Notification';
+import Notification from '@components/notification';
 
 const CreateTask = () => {
   const { data: session } = useSession();
