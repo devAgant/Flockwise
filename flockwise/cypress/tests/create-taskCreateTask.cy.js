25fbcc38b13bdb3793b7772ffd9b763716b2f81d
Arif <68075877+devAgant@users.noreply.github.com>
2023-11-20 04:36:50 -0600

    deleted random ass file

--- flockwise/cypress/tests/create-taskCreateTask.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CreateTask from '../../app/task-manager/create-task'
-
-describe('<CreateTask />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<CreateTask />)
-  })
-})
